7a30e58a57561019e559939c10987ffb6d982b2b
Must_nu73 <Must_nu73@STHQ0122-08>
2021-11-13 20:14:47 +0400

    Add project files.

---
+++ AdoNetTask1/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using AdoNetTask1.Command;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdoNetTask1.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainWindow MainView { get; set; }
+
+        public RelayCommand ShowAllCommand { get; set; }
+
+        SqlConnection conn;
+
+        string cs = "";
+
+        DataSet set;
+
+        SqlDataAdapter dataAdapter;
+
+        public MainViewModel()
+        {
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+
+            ShowAllCommand = new RelayCommand(sender =>
+            {
+                using (conn = new SqlConnection())
+                {
+                    conn.ConnectionString = cs;
+                    conn.Open();
+                    set = new DataSet();
+                    dataAdapter = new SqlDataAdapter("SELECT * FROM Books;", conn);
+
+                    dataAdapter.Fill(set, "mybook");
+                    MainView.MyDataGrid.ItemsSource = set.Tables[0].DefaultView;                                     
+                }
+
+            });
+
+
+
+        }
+
+    }
+
+}
